4da4bc99e89896401e19ba35a10cbbb5b8608fa1
Joshua Froggatt <jfrogg.au@gmail.com>
2024-03-24 14:50:22 +1030

    modified:   COMP3023-Assignment1/Comp3023Assignment1/ItemManager.h add maps to the ItemManager and created the RegisterItem function

--- COMP3023-Assignment1/Comp3023Assignment1/ItemManager.h
+++ COMP3023-Assignment1/Comp3023Assignment1/ItemManager.h
@@ -1,14 +1,25 @@
+#include <unordered_map>
 #include <map>
+#include <iostream>
+#include <memory>
 
+#include "AbstractItem.h"
 
 #ifndef ITEMMANAGER
 #define ITEMMANAGER
 
 class ItemManager {
 public:
+	//registers the item by adding it to the map
+	void RegisterItem(AbstractItem* new_item);
+
 
 private: 
-	
+	//keeps track of the items
+	std::map<std::string, AbstractItem*> items;
+	//system_multiplers < multiplier name, multipler>
+	//e.g. system_multipliers < Scrap Value Multiplier, 1.10 >
+	std::unordered_multimap<std::string, float> system_multipliers;
 };
 
 #endif
